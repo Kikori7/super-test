2c8be7f8d8e484a657c9f1e65d5512f7399b7d5b
axiaoli <1452066366@qq.com>
2025-12-18 16:54:17 +0800

    feat:ci init

---
+++ src/test/java/org/test/normal/Test.java
@@ -0,0 +1,38 @@
+package org.test.normal;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+/**
+ * @author Kiko Song
+ * @create 2025-12-18 15:02
+ */
+public class Test {
+    private Browser browser;
+    private Page page;
+    @BeforeClass
+    public void setUp() {
+        Playwright playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        page = browser.newPage();
+    }
+
+    @org.testng.annotations.Test
+    public void testHomePageTitle() {
+        page.navigate("https://baidu.com");
+        String title = page.title();
+        Assert.assertEquals(title, "百度一下，你就知道");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (browser != null) {
+            browser.close();
+        }
+    }
+}
